2e8638ca79e7f7743788ff55d59caeac3d51546e
Barkley Santo <barkley.santo@gmail.com>
2021-05-03 12:24:12 -0500

    added BB logo from cloudinary

---
+++ src/components/ui/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+// import logo from '../../img/logo'
+
+const Header = () => {
+  return (
+    <header className='center'>
+      <img src='https://res.cloudinary.com/duj93wpnu/image/upload/v1620062622/d6j1jrs-c16bbc3d-0980-4ca5-badc-c173ce1b0219_jtlt78.jpg' alt='logo' />
+    </header>
+  )
+}
+
+export default Header;
