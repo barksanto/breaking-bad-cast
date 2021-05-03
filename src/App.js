13876190380a8b795d93adf846c1701179a4278c
Barkley Santo <barkley.santo@gmail.com>
2021-05-03 12:45:53 -0500

    create characterGrid and characterItem-- pass data through CharacterGrid to CharItem through props - CharItem renders with passes down data

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import './App.css';
 import Header from './components/ui/Header'
 import axios from 'axios';
+import CharacterGrid from './components/characters/CharacterGrid'
 
 const App = () => {
 
@@ -24,6 +25,7 @@ const App = () => {
   return (
     <div className="container">
       <Header />
+      <CharacterGrid isLoading={isLoading} items={items} />
     </div>
   );
 }
